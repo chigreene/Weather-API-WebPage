b5d8d9ec859bd4b75a0d46c3e0d7cdd48664f5a4
Chirs Greene <chigreenwork@gmail.com>
2023-08-24 10:02:17 -0400

    weather is dynamically rendered for selected city

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,24 @@ var state;
 var country;
 var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
 
+var root = document.getElementById('todaysWeather');
+
 fetch(queryURL)
     .then(function (response){
         return response.json()
     })
     .then(function (data){
         console.log(data)
+        
+        var temp = document.createElement('p')
+        var wind = document.createElement('p')
+        var humidity = document.createElement('p')
+        
+        temp.textContent = 'Temp: ' + data.main.temp + ' degrees Kelvin';
+        wind.textContent = 'Wind: ' + data.wind.speed + 'mph';
+        humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
+
+        root.append(temp)
+        root.append(wind)
+        root.append(humidity)
     })

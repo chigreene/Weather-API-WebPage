70d1a0eb99ff7d7faa513bcab96b3a299578cbcc
Chirs Greene <chigreenwork@gmail.com>
2023-08-24 11:46:03 -0400

    search button works now. The weather is displayed for the inputed city

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,36 @@
 var APIKey = '3b136a65d0153eeff26cb38c4e78b611'
-var city = "Atlanta";
+var city = 'Hartford';
 var state;
 var country;
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
 
-var root = document.getElementById('todaysWeather');
+var root = document.getElementById('todaysWeather')
 
-fetch(queryURL)
-    .then(function (response){
-        return response.json()
-    })
-    .then(function (data){
-        console.log(data)
-        
-        var temp = document.createElement('p')
-        var wind = document.createElement('p')
-        var humidity = document.createElement('p')
-        
-        temp.textContent = 'Temp: ' + data.main.temp + ' degrees Kelvin';
-        wind.textContent = 'Wind: ' + data.wind.speed + 'mph';
-        humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
+// button
+var btn = document.querySelector('#selectCityBtn')
+var cityInput = document.querySelector('#citySelector')
 
-        root.append(temp)
-        root.append(wind)
-        root.append(humidity)
-    })
+btn.addEventListener("click", function(){
+    var selectedCity = cityInput.value
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + selectedCity + '&appid=' + APIKey;
+
+// fetch query
+    fetch(queryURL)
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (data){
+            console.log(data)
+            
+            var temp = document.createElement('p')
+            var wind = document.createElement('p')
+            var humidity = document.createElement('p')
+            
+            temp.textContent = 'Temp: ' + Math.floor((data.main.temp - 273.15) * 1.8 + 32) + ' degrees Kelvin';
+            wind.textContent = 'Wind: ' + data.wind.speed + 'mph';
+            humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
+
+            root.append(temp)
+            root.append(wind)
+            root.append(humidity)
+        });
+})

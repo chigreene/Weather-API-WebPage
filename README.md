ccafd6bc25aa2c90581d8d76950919850c14180e
Chirs Greene <chigreenwork@gmail.com>
2023-08-30 12:04:11 -0400

    fixes night icons not showing up

---
+++ README.md
@@ -0,0 +1,19 @@
+# Weather API WebPage
+
+## Description
+
+The website provides the weather today and a five day forecast. Each searched city is saved to a favorites bar to provide easy access to past searches. 
+
+- My motivation was to used my newly learned skills with local storage, data-indexes, and API fetches to solved a practical problem; knowing the weather for the week.
+- I built this project because it a doable project to tackle and can be solved with the skills I have learned over the past two months.
+- This project solves the problem of not knowing what the weather is like outside or for the next 4 days.
+- What I learned is how to use local storage to create a history tab, how to use multiple API's and how to use function to create logic and control the flow of the program. 
+
+Link to webpage: https://chigreene.github.io/Weather-API-WebPage/
+
+![Weather API Screenshot](./assets/Weather%20API.png)
+
+## Credits
+Ross,
+Matt,
+EdX
--- assets/script.js
+++ assets/script.js
@@ -132,6 +132,15 @@ function apiCall(city){
                             '11d' : 'https://openweathermap.org/img/wn/11d@2x.png',
                             '13d' : 'https://openweathermap.org/img/wn/13d@2x.png',
                             '50d' : 'https://openweathermap.org/img/wn/50d@2x.png',
+                            '01n' : 'https://openweathermap.org/img/wn/01n@2x.png',
+                            '02n' : 'https://openweathermap.org/img/wn/02n@2x.png',
+                            '03n' : 'https://openweathermap.org/img/wn/03n@2x.png',
+                            '04n' : 'https://openweathermap.org/img/wn/04n@2x.png',
+                            '09n' : 'https://openweathermap.org/img/wn/09n@2x.png',
+                            '10n' : 'https://openweathermap.org/img/wn/10n@2x.png',
+                            '11n' : 'https://openweathermap.org/img/wn/11n@2x.png',
+                            '13n' : 'https://openweathermap.org/img/wn/13n@2x.png',
+                            '50n' : 'https://openweathermap.org/img/wn/50n@2x.png',
                         }
             
                         iconURL = iconCodes[icon]
